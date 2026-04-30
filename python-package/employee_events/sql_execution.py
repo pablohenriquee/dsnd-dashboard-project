a632de64756cb1e77373584e169acd1eb5c504d1
pablohenriquee <120510722+pablohenriquee@users.noreply.github.com>
2026-04-30 14:16:27 -0300

    Update sql_execution.py

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
+db_path = Path("employee_events.db").absolute()
 
 
 # OPTION 1: MIXIN
@@ -16,16 +16,20 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(self, sql_query):
+        with connect(db_path) as conn:
+            return pd.read_sql_query(sql query, conn)
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
-    
-
+    def query(self, sql_query):
+        with connect(db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            return cursor.fetchall() 
  
  # Leave this code unchanged
 def query(func):
